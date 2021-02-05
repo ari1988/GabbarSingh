d36b212760bf56acd654474c34f035251b616fa8
Arvind Singh <Arvind.Singh@web.com>
2021-02-05 21:51:41 +0530

    IMPROVE: Clears Console

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+const clearConsole = require('clear-any-console')
+
+//Clears the Console
+clearConsole();
 
 console.log(`
 Hi there, 👋 I am Arvind Singh
@@ -9,6 +13,7 @@ Talk to me about : Kubernetes, Docker , Nginx, Nagios, Test Automation
 🤝 Reach me at : 
 🐦 Twitter  https://twitter.com/0xArvind
 📖 Github   https://github.com/ari1988
+🥡 LinkedIn https://www.linkedin.com/in/arvindsingh88/
 🦖 Game     https://ari1988.github.io/
 
 `);
