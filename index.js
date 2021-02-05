0ab3a97f72b622520e2223cb69240d553d928dd4
Arvind Singh <Arvind.Singh@web.com>
2021-02-05 19:10:54 +0530

    👌 IMPROVE: first Version

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log(`Arvind Singh`);
+
+console.log(`
+Hi there, 👋 I am Arvind Singh
+
+DevOps Automation Enginner.My Motto: "Learn, Break and implement" 😎 
+Talk to me about : Kubernetes, Docker , Nginx, Nagios, Test Automation
+
+🤝 Reach me at : 
+🐦 Twitter  https://twitter.com/0xArvind
+📖 Github   https://github.com/ari1988
+🦖 Game     https://ari1988.github.io/
+
+`);
